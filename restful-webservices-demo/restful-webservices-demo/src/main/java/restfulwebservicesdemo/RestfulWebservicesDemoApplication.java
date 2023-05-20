39d733a9c08e9c466d43f266bb7cf1d31661ef5d
harish5058 <hariharsangale@gmail.com>
2023-05-20 20:05:38 +0530

    rest project

---
+++ restful-webservices-demo/restful-webservices-demo/src/main/java/restfulwebservicesdemo/RestfulWebservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package restfulwebservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesDemoApplication.class, args);
+	}
+
+}
